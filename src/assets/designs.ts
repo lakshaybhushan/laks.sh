fb8d59871053a9abf9e20b075800a392351347c5
Lakshay Bhushan <thegreenfoxyit@gmail.com>
2025-03-29 00:30:13 +0530

    testing cdn for images  (P1.1)

--- src/assets/designs.ts
+++
@@ -1,8 +0,0 @@
-import designsjson from "../constants/designs.json";
-
-export const designs = designsjson.map((design) => {
-	return {
-		...design,
-		image: `/src/assets/designs/${design.imageKey}.webp`,
-	};
-});
