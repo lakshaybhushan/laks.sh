6777e3a9b920a3ace464b1c7966ef054feab36ce
Lakshay Bhushan <hi@laks.sh>
2026-02-22 22:10:16 +0100

    rm -rf video

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 coming soon ™
-
-<video src="./public/wait.mp4" autoplay muted playsinline loop controls style="max-width: 100%;"></video>
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { DelayedVideo } from "../components/DelayedVideo";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center md:p-24 md:gap-8 gap-4 px-4 py-16">
-      <p>Work in progress, please check back later :D</p>
-      <DelayedVideo />
+      <p></p>
     </main>
   );
 }
